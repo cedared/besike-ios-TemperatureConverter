3b6b6af17f894827208e9fc7d02c9c0ab884ff1a
Albert Young <yangsong@2bapm.com>
2014-10-24 13:41:23 +0800

    Initial Commit

---
+++ Temperature Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Temperature Converter
+//
+//  Created by Albert Young on 14/10/24.
+//  Copyright (c) 2014年 2bapm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Temperature ConverterTests/Temperature_ConverterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Temperature_ConverterTests.swift
+//  Temperature ConverterTests
+//
+//  Created by Albert Young on 14/10/24.
+//  Copyright (c) 2014年 2bapm. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Temperature_ConverterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
