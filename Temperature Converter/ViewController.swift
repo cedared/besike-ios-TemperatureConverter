8036f80c2b3e6c5bff072269e8bbc8f66c6ae877
Albert Young <yangsong@2bapm.com>
2014-11-01 14:59:10 +0800

    设置自适应

--- Temperature Converter/ViewController.swift
+++ Temperature Converter/ViewController.swift
@@ -9,10 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var kelvinLabel: UILabel!
+    @IBOutlet weak var temperatureSlider: UISlider!
+    
+    func updateTemperatureDisplay(){
+        var celsiusValue: Float = temperatureSlider.value
+        var fahrenheitValue: Float = (celsiusValue * 9/5) + 32
+        var kelvinValue: Float = celsiusValue + 273.15
+        
+        celsiusLabel.text = String(format: "%.1f", arguments: [celsiusValue]) + "C"
+        fahrenheitLabel.text = String(format: "%.1f", arguments: [fahrenheitValue]) + "F"
+        kelvinLabel.text = String(format: "%.2f", arguments: [kelvinValue]) + "K"
+    }
+    
+    
+    @IBAction func temperatureSliderChanged(sender: UISlider){
+        self.updateTemperatureDisplay()
+        
+    }
+    
+//    override func viewWillLayoutSubviews() {
+//        super.viewWillLayoutSubviews()
+//        let screen = UIScreen.mainScreen()
+//        let screenWidth = screen.bounds.width
+//        let screenHight = screen.bounds.height
+//        let screenTopLayout = topLayoutGuide.length
+//        
+//        titleLable.frame = CGRect(x: 16,y: (screenTopLayout + 16),width: (screenWidth - 32),height: 21)
+//        titleLable.backgroundColor = UIColor.grayColor()
+//        titleLable.textAlignment = NSTextAlignment.Center
+//        // Always return the size of the screen in portraits orientation
+//        println("Screen fixed bounds: \(screen.fixedCoordinateSpace.bounds.width)")
+//        // Since iOS8, this is the size of the screen in its current orientation
+//        println("Screen bounds: \(screen.bounds)")
+//        
+//        println("Top layout guide: \(topLayoutGuide.length)")
+//    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        temperatureSlider.minimumValue = 0
+        temperatureSlider.maximumValue = 100
+        temperatureSlider.value = 30
+        
     }
 
     override func didReceiveMemoryWarning() {
